0e8843543c2c48dc0f972a65f9a7ad56b7bb6941
ayakend <ayakend@gmail.com>
2023-02-09 23:29:16 +0600

    lesson7

---
+++ app/src/main/java/com/example/taskapp/data/remote/firestore/FirestoreUtils.kt
@@ -0,0 +1,7 @@
+package com.example.taskapp.data.remote.firestore
+
+import com.google.firebase.firestore.FirebaseFirestore
+
+class FirestoreUtils {
+    val fireStore = FirebaseFirestore.getInstance()
+}
